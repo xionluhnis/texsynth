ba2c4b642283c76874f59cc6f8ff0128f4b90b14
Alexandre Kaspar <xion.luhnis@gmail.com>
2014-04-18 19:45:39 +0200

    List with exemplars

--- index.php
+++ index.php
@@ -20,7 +20,19 @@ if (isset($_GET['path'])){
   $target = 'page.html';
 
   if (isset($_GET['params'])) {
-    parse_str($_GET['params'], $params);
+    // manually parse
+    $tokens = explode('/', $_GET['params']);
+    foreach($tokens as $token){
+      $token_parts = explode(':', $token, 2);
+      switch(count($token_parts)){
+      case 2:
+        $params[$token_parts[0]] = $token_parts[1];
+        break;
+      case 1:
+        $params[$token_parts[0]] = '';
+        break;
+      }
+    }
   }
 }
 
@@ -34,7 +46,8 @@ if (!preg_match('![0-9]{4}/.+!', $path) || !is_dir($path)){
 // set environment variables
 $env = array(
   'path' => $path,
-  'params' => $params
+  'params' => $params,
+  'options' => array('big', 'small')
 );
 
 include_once 'libs/index.inc.php';
@@ -49,21 +62,71 @@ switch($target) {
     $env['text']  = get_event_text($path);
     $env['title'] = get_event_title($path, 'Synthesis: ' . $path);
 
+    // load the display parameters
+    static $display_modes = array('big', 'small', 'list', 'video');
+    $env['class'] = 'small';
+    foreach($params as $pname => $pval){
+      if(in_array($pname, $display_modes)){
+        $env['class'] = $pname;
+        break;
+      }
+    }
+
     // load the event structure data
-    parse_str(get_file_content($path), $data);
+    parse_str(get_file_content($path . '/data.dat'), $data);
     $env['data']  = $data;
-    $type = array_key_exists($data, 'type') ? $data['type'] : 'single';
+    $type = array_key_exists('type', $data) ? trim($data['type']) : 'single';
 
     switch($type) {
       case 'single':
         $env['images'] = get_event_images($path);
         break;
+      case 'default':
+        $env['images'] = get_event_images($path, '-im'); // only select the ones with filename ending in -im
+        $env['options'][] = 'list';
+        switch($env['class']){
+          case 'list':
+            if(!empty($env['images'])){
+              // add the current picture
+              if(array_key_exists('image_base', $params)){
+                $base = $params['image_base'];
+                // find the image
+                foreach($env['images'] as $img){
+                  $file = pathinfo($img);
+                  if($file['filename'] === $base . '-im') {
+                    $env['image'] = get_imageinfo($img);
+                    $env['image_base'] = $base;
+                    break;
+                  }
+                }
+              } else {
+                $file = get_pathinfo(reset($env['images']));
+                $env['image'] = $file;
+                $name = $file['filename'];
+                $env['image_base'] = substr($name, 0, strlen($name) - 3); // remove -im
+              }
+              // Check for exemplar
+              $ex = get_image_exemplar($env['image']['file']);
+              if(!empty($ex)){
+                $env['exemplar'] = get_imageinfo($ex);
+              }
+            } else {
+              $env['class'] = 'small'; // revert else illconditioned
+            }
+            break;
+          default:
+        }
+        break;
     }
     break;
 }
 
-$twig->addFilter(new Twig_SimpleFilter('filename', function($file){
-  return pathinfo($file, PATHINFO_FILENAME);
+$twig->addFilter(new Twig_SimpleFilter('filename', function($file, $args){
+  $name = pathinfo($file, PATHINFO_FILENAME);
+  if(isset($args) && substr($name, -strlen($args)) === $args){
+    $name = substr($name, 0, -strlen($args));
+  }
+  return $name;
 }));
 
 // render!
--- libs/page.inc.php
+++ libs/page.inc.php
@@ -8,12 +8,43 @@ function get_event_text($path) {
   return $md->text($text);
 }
 
-function get_event_images($path) {
+function get_pathinfo($path) {
+  $info = pathinfo($path);
+  $info['file'] = $path;
+  return $info;
+}
+
+function get_imageinfo($file) {
+  $info = get_pathinfo($file);
+  list($width, $height, $tail) = getimagesize($file);
+  $info['width'] = $width;
+  $info['height'] = $height;
+  return $info;
+}
+
+function get_event_images($path, $suffix = '') {
   static $image_extensions = array('png', 'jpg');
-  $files = array_filter(glob($path . '/images/*'), function($file){
+  $files = array_filter(glob($path . '/images/*'), function($file) use(&$suffix) {
+    if (strlen($suffix) > 0) {
+      $name = pathinfo($file, PATHINFO_FILENAME);
+      if (substr($name, -strlen($suffix)) !== $suffix) return false;
+    }
     return is_file($file) || in_array(pathinfo($file, PATHINFO_EXTENSION), $image_extensions);
   });
   return $files;
+}
+
+function get_image_exemplar($file) {
+  static $images_extensions = array('png', 'jpg');
+  $info = pathinfo($file);
+  $name = $info['filename'];
+  $ex_base = $info['dirname'] . '/' . str_replace('-im', '-ex.', $name);
+  foreach($images_extensions as $fext){
+    if(file_exists($ex_base . $fext)){
+      return $ex_base . $fext;
+    }
+  }
+  return '';
 }
 
 ?>
